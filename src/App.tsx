289f7836434047e2634a729752128437fabf6206
Rohith Gilla <gillarohith1@gmail.com>
2022-02-12 14:09:06 +0530

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import "./App.css";
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <h1 className="bg-slate-500 font-bold underline">
-      Hello Vite + Tauri + Tailwind + React
-    </h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
